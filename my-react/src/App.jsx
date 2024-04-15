409de81438c04403bdc447dca3c196c646d6e560
Khaista-Rahman <Khaistarahman.cs@gmail.com>
2024-04-15 17:09:09 +0000

    learnigfrom youtube

---
+++ my-react/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-pink-500 text-white rounded-xl p-4'>
+        this is khaista rahman
+      </h1>
+    </>
+  )
+}
+
+export default App
