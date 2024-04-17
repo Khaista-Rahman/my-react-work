d43eda2d0792cd0a459623d37b3c0cfc5b4ef119
Khaista-Rahman <Khaistarahman.cs@gmail.com>
2024-04-17 17:52:05 +0000

    small work but new

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Chai from './Chai.jsx'; // Remove whitespace between '/src' and '/Chai.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className='bg-pink-500 text-white rounded-xl p-4'>
-        this is khaista rahman
-      </h1>
-    </>
-  )
+    <Chai />
+  );
 }
 
-export default App
+export default App;
---
+++ my-react/src/Chai.jsx
@@ -0,0 +1,10 @@
+
+
+
+function Chai(){
+  return (
+    <h1 className="bg-slate-950 text-white rounded-xl p-7">chai is ready</h1>
+  )
+}
+
+export default Chai;
